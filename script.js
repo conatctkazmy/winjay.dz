c174c38ba8f76df9c1af7ffb5a413c124e07f41a
conatctkazmy <contactkazmy@gmail.com>
2026-05-20 06:41:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4984,7 +4984,7 @@ function populateAllExtraCategories() {
 function openOtherCategoriesModal(clearTarget = false) {
     if (clearTarget) categoryPickerTargetSelectId = '';
     try {
-        null;
+        closeSidebarOverlay();
     } catch (e) {
         null;
     }
